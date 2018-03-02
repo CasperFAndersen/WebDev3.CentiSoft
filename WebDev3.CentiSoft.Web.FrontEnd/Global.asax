b05a956dc99d80ffd545183aaa84cf19aed2deca
Casper Froberg Andersen <casperfroberg@gmail.com>
2018-03-02 09:46:59 +0100

    initial comit

---
+++ WebDev3.CentiSoft.Web.FrontEnd/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebDev3.CentiSoft.Web.FrontEnd.MvcApplication" Language="C#" %>
---
+++ WebDev3.CentiSoft.Web.FrontEnd/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebDev3.CentiSoft.Web.FrontEnd.Startup))]
+namespace WebDev3.CentiSoft.Web.FrontEnd
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
